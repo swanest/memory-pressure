fa6cc7e4b87341411fadc7abe57f7446a0a68371
Cyril Lequeux <l.cyril@gmail.com>
2016-09-19 17:30:18 +0200

    chore(): init

---
+++ test/memory.js
@@ -0,0 +1,36 @@
+var pressure = require("../lib/index");
+var _ = require("lodash");
+var expect = require("chai").expect;
+var fs = require("fs");
+
+describe("When monitoring memory pressure", function () {
+
+    it("monitors", function (done) {
+        this.timeout(400000);
+        var buff = [], t = null, states = [];
+        pressure.on("underPressure", function (memoryUsage) {
+            states.push(0);
+            buff = [];
+            setImmediate(global.gc);
+            clearInterval(t);
+        });
+        pressure.on("pressureReleased", function (memoryUsage) {
+            states.push(1);
+            setup();
+        });
+        pressure.on("manualReleaseRequired", _.noop);
+
+        function setup() {
+            t = setInterval(function () {
+                buff.push(JSON.parse(fs.readFileSync(__dirname + "/data.json")));
+            }, 1);
+        };
+        setup();
+        setTimeout(function () {
+            expect(states.length >= 2).to.be.ok;
+            expect(states[0]).to.equal(0);
+            expect(states[1]).to.equal(1);
+            done();
+        }, 30000);
+    });
+});
